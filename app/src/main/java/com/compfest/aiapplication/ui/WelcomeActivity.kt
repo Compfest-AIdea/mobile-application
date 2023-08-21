dba28df7f6bae464814d7881b7465e0de96d9eae
lckmnzans <lckmnzans@gmail.com>
2023-08-21 14:30:29 +0700

    add home activity

--- app/src/main/java/com/compfest/aiapplication/ui/WelcomeActivity.kt
+++ app/src/main/java/com/compfest/aiapplication/ui/WelcomeActivity.kt
@@ -1,12 +1,26 @@
 package com.compfest.aiapplication.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.compfest.aiapplication.R
+import com.compfest.aiapplication.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnNext.setOnClickListener {
+            intent = Intent(this, HomeActivity::class.java)
+            getUserName(intent)
+            startActivity(intent)
+        }
+    }
+
+    private fun getUserName(intent: Intent) {
+        intent.putExtra("Name", binding.etName.text.toString())
+        intent.putExtra("NickName", binding.etNickname.text.toString())
     }
 }
